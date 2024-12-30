421d7f3c8012e596f82579c01a76165ac065ab1a
GraydonvG <graydonvg@gmail.com>
2024-12-30 10:14:57 +0200

    fix paragraph missing from about me background card

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -45,7 +45,7 @@ export const aboutItems = [
   {
     title: "Background",
     parapraphs:
-      "I started my career in hospitality as a restaurant manager, but I soon craved more intellectual challenges.<br />My love for problem-solving, sparked by high school calculus, led me to explore applied mathematics and programming languages such as C++, MATLAB, and R.<br />Curiosity then brought me to web development, where I began with HTML, CSS, and JavaScript, eventually falling in love with React and Next.js.",
+      "I started my career in hospitality as a restaurant manager, but I soon craved more intellectual challenges.<br />My love for problem-solving, sparked by high school calculus, led me to explore applied mathematics and programming languages such as C++, MATLAB, and R.<br />Curiosity then brought me to web development, where I began with HTML, CSS, and JavaScript, eventually falling in love with React and Next.js.<br />I thrive on the creative problem-solving that coding offers, and I'm always excited to tackle new challenges.",
   },
   {
     title: "Expertise",
