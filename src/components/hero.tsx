9c269e513511612d463a0a484db770ea578a3905
GraydonvG <graydonvg@gmail.com>
2024-12-31 14:34:34 +0200

    make hero height responsive

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -10,12 +10,12 @@ export default function Hero() {
   return (
     <section
       id="hero"
-      className="relative min-h-screen overflow-clip bg-[linear-gradient(to_bottom,black,#071E18_35%,#208A65_67%,#35Fb8E_85%)]"
+      className="relative min-h-screen overflow-hidden bg-[linear-gradient(to_bottom,black,#071E18_35%,#208A65_67%,#35Fb8E_85%)]"
     >
-      <div className="absolute left-1/2 top-[52%] min-h-screen w-[200vw] min-w-[2000px] -translate-x-1/2 rounded-[50%] border-[1px] border-[#8CD6DE]/30 bg-[radial-gradient(closest-side,black_85%,#249974)]" />
+      <div className="absolute left-1/2 top-[50%] min-h-screen w-[200vw] min-w-[2000px] -translate-x-1/2 rounded-[50%] border-[1px] border-[#8CD6DE]/30 bg-[radial-gradient(closest-side,black_85%,#249974)]" />
 
-      <div className="absolute left-0 top-0 size-full">
-        <div className="flex size-full flex-col items-center justify-center gap-8 text-center sm:gap-14">
+      <div className="relative flex min-h-screen w-full flex-col items-center justify-center md:py-24">
+        <div className="flex w-full flex-col items-center gap-8 text-center sm:gap-14 lg:gap-[clamp(1rem,5.6vh,3.5rem)]">
           <motion.div
             initial={{ opacity: 0, y: -20 }}
             animate={{ opacity: 1, y: 0 }}
@@ -25,15 +25,16 @@ export default function Hero() {
               Hello, I&apos;m Graydon!
             </p>
           </motion.div>
+
           <motion.div
             initial={{ opacity: 0, scale: 0.5 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: 0.8 }}
-            className="relative"
+            className="relative aspect-square h-[250px] sm:h-[350px] md:h-[25vh] md:max-h-[250px] md:max-w-[250px]"
           >
-            <div className="absolute inset-0 overflow-hidden rounded-[50%] bg-gradient-to-b from-emerald-500/20 to-transparent blur-3xl" />
+            <div className="absolute inset-0 rounded-[50%] bg-gradient-to-b from-emerald-500/20 to-transparent blur-3xl" />
 
-            <div className="relative aspect-square w-[250px] overflow-hidden rounded-[50%]">
+            <div className="relative aspect-square overflow-hidden rounded-[50%]">
               <Image
                 src={headshot}
                 alt="headshot"
@@ -41,7 +42,7 @@ export default function Hero() {
                 placeholder="blur"
                 priority
                 fill
-                sizes="250px"
+                sizes="(min-width: 1880px) 250px, (min-width: 780px) calc(12.96vw + 9px), (min-width: 640px) 350px, 250px"
                 className="object-cover object-[50%_0%]"
               />
             </div>
@@ -51,7 +52,7 @@ export default function Hero() {
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8 }}
-            className="space-y-8 sm:space-y-14"
+            className="z-10"
           >
             <TypographyH1>
               Front-end <span className="hidden sm:inline">web</span> developer
@@ -60,6 +61,14 @@ export default function Hero() {
               <br className="sm:hidden" />{" "}
               <span className="text-accent">ReactJS.</span>
             </TypographyH1>
+          </motion.div>
+
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}
+            className="z-10"
+          >
             <a
               tabIndex={-1}
               href="/docs/resume.pdf"
--- src/components/ui/typography/h1.tsx
+++ src/components/ui/typography/h1.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function TypographyH1({ children }: { children: ReactNode }) {
   return (
-    <h1 className="text-h1-sm font-extrabold tracking-tight sm:text-h1-lg/tight">
+    <h1 className="text-h1-sm/tight font-extrabold tracking-tight sm:text-h1-lg/tight">
       {children}
     </h1>
   );
