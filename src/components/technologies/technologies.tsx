a730d12a63b07044fab1308ba38315972d73709b
GraydonvG <graydonvg@gmail.com>
2025-01-01 13:56:25 +0200

    refactor technologies animation

--- src/components/technologies/technologies.tsx
+++ src/components/technologies/technologies.tsx
@@ -8,63 +8,49 @@ import TypographyH2 from "../ui/typography/h2";
 import Section from "../ui/section";
 import TypographyP from "../ui/typography/p";
 import Technology from "./technology";
+import useScrollDirection from "@/hooks/use-scroll-direction";
+import { useState } from "react";
 
 if (typeof window !== "undefined") {
   gsap.registerPlugin(ScrollTrigger, useGSAP);
 }
 
 export default function Technologies() {
+  const scrollDirection = useScrollDirection();
+  const [isInView, setIsInView] = useState(false);
+
   useGSAP(
     () => {
-      gsap
-        .timeline({
-          scrollTrigger: {
-            trigger: "#technologies-grid",
-            start: "top bottom",
-            end: "bottom top",
-            toggleActions: "play reset none reset",
-          },
-        })
-        .fromTo(
-          ".technology",
-          { scale: 0, y: 100 },
-          {
-            scale: 1,
-            y: 0,
-            ease: "power1.out",
-            stagger: {
-              amount: 0.5,
-              grid: "auto",
-              from: "start",
-            },
-          },
-        );
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: "#technologies-grid",
+          start: "top bottom",
+          end: "bottom top",
+          toggleActions: "play reset play reset",
+          onEnter: () => setIsInView(true),
+          onEnterBack: () => setIsInView(true),
+          onLeave: () => setIsInView(false),
+          onLeaveBack: () => setIsInView(false),
+        },
+      });
 
-      gsap
-        .timeline({
-          scrollTrigger: {
-            trigger: "#technologies-grid",
-            start: "top bottom",
-            end: "bottom top",
-            toggleActions: "none reset play reset",
+      if (isInView) return;
+
+      tl.fromTo(
+        ".technology",
+        { y: scrollDirection === "down" ? 100 : -100, scale: 0 },
+        {
+          y: 0,
+          scale: 1,
+          ease: "power1.out",
+          stagger: {
+            amount: 0.5,
+            from: scrollDirection === "down" ? "start" : "end",
           },
-        })
-        .fromTo(
-          ".technology",
-          { scale: 0, y: -100 },
-          {
-            scale: 1,
-            y: 0,
-            ease: "power1.out",
-            stagger: {
-              amount: 0.5,
-              grid: "auto",
-              from: "end",
-            },
-          },
-        );
+        },
+      );
     },
-    { revertOnUpdate: true },
+    { dependencies: [scrollDirection], revertOnUpdate: true },
   );
 
   return (
