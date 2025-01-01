da65e6bc560eeb6541749dcce3697d43e9ada5a5
GraydonvG <graydonvg@gmail.com>
2025-01-01 16:09:28 +0200

    recalculate fixed contact form ScrollTrigger on accordion size change

--- src/components/contact/fixed-contact-form-container.tsx
+++ src/components/contact/fixed-contact-form-container.tsx
@@ -15,37 +15,57 @@ export default function FixedContactFormContainer() {
   const hero =
     typeof window !== "undefined" ? document.getElementById("hero") : null;
 
-  useGSAP(() => {
-    // Mobile devices calculate the browser viewport as (top bar + document + bottom bar) = 100vh
-    // Using hero height (100vh) instead of window height so that it take mobile browser bars into account
-    const windowHeight = hero?.offsetHeight || 0;
-    const footerHeight = contactFormContainerRef?.current?.offsetHeight
-      ? contactFormContainerRef.current.offsetHeight
-      : 0;
+  const accordion =
+    typeof window !== "undefined"
+      ? document.querySelector("#projects-accordion")
+      : null;
 
-    // We never want it to overlap more than the height of the screen
-    function getOverlap() {
-      return Math.min(windowHeight, footerHeight);
-    }
+  useGSAP(
+    () => {
+      if (!contactFormContainerRef.current || !hero) return;
+      const container = contactFormContainerRef.current;
+      // Mobile devices calculate the browser viewport as (top bar + document + bottom bar) = 100vh
+      // Using hero height (100vh) instead of window height so that it take mobile browser bars into account
+      const windowHeight = hero?.offsetHeight || 0;
+      const footerHeight = contactFormContainerRef?.current?.offsetHeight
+        ? contactFormContainerRef.current.offsetHeight
+        : 0;
 
-    // Adjusts the margin-top of the contact section to overlap the proper amount
-    function adjustFooterOverlap() {
-      if (!contactFormContainerRef.current) return;
-      contactFormContainerRef.current.style.marginTop = `-${getOverlap()}px`;
-    }
+      // We never want it to overlap more than the height of the screen
+      function getOverlap() {
+        return Math.min(windowHeight, footerHeight);
+      }
 
-    adjustFooterOverlap(); // Set initial contact section margin
+      // Adjusts the margin-top of the contact section to overlap the proper amount
+      function adjustFooterOverlap() {
+        container.style.marginTop = `-${getOverlap()}px`;
+      }
 
-    gsap.set(contactFormContainerRef.current, {
-      scrollTrigger: {
-        trigger: contactFormContainerRef.current,
+      adjustFooterOverlap(); // Set initial contact section margin
+
+      ScrollTrigger.create({
+        trigger: container,
         start: () => `top ${windowHeight - getOverlap()}`,
         end: () => `+=${getOverlap()}`,
         pin: true,
-        invalidateOnRefresh: true,
-      },
-    });
-  });
+      });
+
+      // Watch for changes in the accordion's height
+      const observer = new ResizeObserver(() => {
+        console.log(true);
+
+        ScrollTrigger.refresh(); // Recalculate ScrollTrigger on accordion size change
+      });
+
+      if (accordion) observer.observe(accordion);
+
+      return () => {
+        observer.disconnect();
+        ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+      };
+    },
+    { dependencies: [accordion], revertOnUpdate: true },
+  );
 
   return (
     <div ref={contactFormContainerRef} className="!w-full !max-w-full bg-card">
