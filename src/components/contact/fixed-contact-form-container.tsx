27ca05aa424740b8ef7fb6da894cc48102d8b21b
GraydonvG <graydonvg@gmail.com>
2025-01-01 16:20:54 +0200

    fix variable names

--- src/components/contact/fixed-contact-form-container.tsx
+++ src/components/contact/fixed-contact-form-container.tsx
@@ -14,51 +14,49 @@ export default function FixedContactFormContainer() {
   const contactFormContainerRef = useRef<HTMLDivElement>(null);
   const hero =
     typeof window !== "undefined" ? document.getElementById("hero") : null;
-
   const projectsAccordion =
     typeof window !== "undefined"
       ? document.querySelector("#projects-accordion")
       : null;
-
   const contactSection =
     typeof window !== "undefined" ? document.querySelector("#contact") : null;
 
   useGSAP(
     () => {
       if (!contactFormContainerRef.current || !hero) return;
-      const container = contactFormContainerRef.current;
+
+      const contactFormContainer = contactFormContainerRef.current;
       // Mobile devices calculate the browser viewport as (top bar + document + bottom bar) = 100vh
       // Using hero height (100vh) instead of window height so that it take mobile browser bars into account
-      const windowHeight = hero?.offsetHeight || 0;
-      const footerHeight = contactFormContainerRef?.current?.offsetHeight
-        ? contactFormContainerRef.current.offsetHeight
-        : 0;
+      const windowHeight = hero.offsetHeight;
+      const contactFormContainerHeight = contactFormContainer.offsetHeight;
 
       // We never want it to overlap more than the height of the screen
       function getOverlap() {
-        return Math.min(windowHeight, footerHeight);
+        return Math.min(windowHeight, contactFormContainerHeight);
       }
 
       // Adjusts the margin-top of the contact section to overlap the proper amount
       function adjustFooterOverlap() {
-        container.style.marginTop = `-${getOverlap()}px`;
+        contactFormContainer.style.marginTop = `-${getOverlap()}px`;
       }
 
       adjustFooterOverlap(); // Set initial contact section margin
 
       ScrollTrigger.create({
-        trigger: container,
+        trigger: contactFormContainer,
         start: () => `top ${windowHeight - getOverlap()}`,
         end: () => `+=${getOverlap()}`,
         pin: true,
       });
 
-      // Watch for changes in the projects accordion height
       const observer = new ResizeObserver(() => {
-        ScrollTrigger.refresh(); // Recalculate ScrollTrigger on projects accordion size change
+        ScrollTrigger.refresh(); // Recalculate ScrollTrigger on element size change
       });
 
+      // Watch for changes in the projects accordion height
       if (projectsAccordion) observer.observe(projectsAccordion);
+      // Watch for changes in the contact section height (form error messages change the height)
       if (contactSection) observer.observe(contactSection);
 
       return () => {
